b03dc41bf0635f56adef535d9c84527b940c5c58
Pierre Neidhardt <mail@ambrevar.xyz>
2018-10-16 12:35:14 +0200

    ascii-strings (FIX)

--- gnu/packages/lisp.scm
+++ gnu/packages/lisp.scm
@@ -31,6 +31,7 @@
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix git-download)
+  #:use-module (guix hg-download)
   #:use-module (guix utils)
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system asdf)
